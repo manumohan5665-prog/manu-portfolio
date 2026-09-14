ea2cae365ee16332c818131f6ef3ca3fd40c5280
Manu Mohandas <manumohan5665@gmail.com>
2026-09-14 23:01:20 +0530

    added navbar section

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import Navbar from "./components/Navbar"
+import "./App.css"
+
+function App() {
+  return (
+    <>
+      <Navbar />
+
+      <div className="hero" id="home">
+        <h1>Manu Mohandas</h1>
+        <p>Full Stack Web Developer</p>
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,21 @@
+function Navbar() {
+  return (
+    <nav className="navbar">
+
+      <div className="logo">
+        MM
+      </div>
+
+      <div className="nav-links">
+        <a href="#home">Home</a>
+        <a href="#about">About</a>
+        <a href="#skills">Skills</a>
+        <a href="#projects">Projects</a>
+        <a href="#contact">Contact</a>
+      </div>
+
+    </nav>
+  )
+}
+
+export default Navbar
